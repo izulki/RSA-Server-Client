45551c62221e658f22a7fbc2d1729803176333a9
Irfan <Irfan@DESKTOP-061P0VF>
2019-03-03 19:53:44 -0400

    Encryption and Decryption added to main menu, debugging removed, encryption and decryption added as functions in lib

--- Client.py
+++ Client.py
@@ -1,17 +1,21 @@
 
-from RSA import generatePrime, generateKey
+from RSA import generatePrime, generateKey, encrypt, decrypt
 import codecs
 import sys
 import binascii
 
 print("Welcome to RSA Messaging System!")
-print("****** Main Menu ******")
+print("****** Client Menu ******")
 print("1. Generate Key Pair")
 print("2. View Key Pair")
-print("3. Send Message")
-print("4. Receive Messages")
+print("3. Encrypt Message")
+print("4. Decrypt Message")
 
-print("****** Debug Menu ******")
+print("\n****** Network Menu ******")
+print("5. Send Message")
+print("6. Receive Messages")
+
+print("\n****** Debug Menu ******")
 print("100. Generate Prime")
 print("101. Encrypt a Message")
 
@@ -21,44 +25,47 @@ userOption = input("Your Option: ")
 if (int(userOption) == 1): 
     generateKey(2048)
 
-if (userOption == 100):
-    print(generatePrime(1024))
+if (int(userOption) == 3):
+    print("Encryption for: ") 
+    print("1. Current Client")
+    print("2. Another Client")
+    encryptionOption = input("Your Option: ")
 
-if (userOption == 101):
-    ###### SHOULD GET PUBLIC KEY HERE ######
-    ## GET ANOTHER CLIENT  n ####
-    file = open("n.txt","r")
-    n = int(file.read())
-    file.close
+    if (encryptionOption == 1):
+        userMessage = raw_input("Enter Message to Encrypt: ")
+        file = open("n.txt","r")
+        n = int(file.read())
+        file.close()
+        e = 5
+        print(encrypt(e,n,userMessage))
 
-    ### GET ANOTHER CLIENT e ###
-    e = 5
-    
-    userMsg_str = raw_input("Enter a message: ")
-    userMsg_intArr = [(ord(c)) for c in userMsg_str] #list compression, convert each character to unicode (Decimal)
-    print(userMsg_intArr)
-    userMsg_hexStr = "0x" + binascii.hexlify(bytearray(userMsg_intArr))
-    print(userMsg_hexStr)
-    userMsg_bigInt = int(userMsg_hexStr, 16)
-    print(userMsg_bigInt)
-    userMsg_ctext =  pow(userMsg_bigInt, e, n)
-    print(userMsg_ctext)
 
-    # Testing for Decreyption
-    file = open("d5.txt","r")
-    d5 = int(file.read())
-    file.close
+    if (encryptionOption == 2):
+        userMessage = input("Enter Message to Encrypt: ")
+        user_e = input("Enter Public Exponent: ")
+        user_n = input("Enter Modulus: ")
 
-    ptext = pow(userMsg_ctext, d5, n)
-    print(ptext)
-    decode_hexStrRaw = hex(ptext) #string
-    print(decode_hexStrRaw)
-    decode_hexStr = decode_hexStrRaw[2:-1]
-    print(decode_hexStr)
+if (int(userOption) == 4):
+    print("4 for: ") 
+    print("1. Current Client")
+    print("2. Another Client")
+    encryptionOption = input("Your Option: ")
 
-    msg = bytearray.fromhex(decode_hexStr)
-    print(msg)
+    if (encryptionOption == 1):
+        userMessage = raw_input("Enter Ciphertext to Decrypt: ")
+        file = open("n.txt","r")
+        n = int(file.read())
+        file.close()
+        file = open("d5.txt","r")
+        d5 = int(file.read())
+        file.close()
+        print(decrypt(n,d5,userMessage))
 
+
+    if (encryptionOption == 2):
+        userMessage = input("Enter Message to Encrypt: ")
+        user_e = input("Enter Public Exponent: ")
+        user_n = input("Enter Modulus: ")
 
     
 
--- RSA.py
+++ RSA.py
@@ -2,6 +2,7 @@ from fractions import gcd
 from random import getrandbits, randrange
 from RM import isPrime, rabinMiller
 from RSAMath import egcd, modinv
+import binascii
 
 #GeneratePrime (RSA) 
 def generatePrime(primeSize): #size of prime in bits 
@@ -16,6 +17,10 @@ def generatePrime(primeSize): #size of prime in bits
                 pass
 
 ### RSA Functions ###
+
+# Function: Generate Keys
+    # Input: Modulus Size
+    # Output: p, q, n, d3, d5 
 def generateKey(modSize):
     p = 0
     q = 0
@@ -68,6 +73,29 @@ def generateKey(modSize):
             file.write(str(d5))
             file.close
 
+# Function: Encrypt Message
+    # Input: Public Exponent, Modulus, Message (Plaintext)
+    # Output: Encrypted Message (Ciphertext)
+def encrypt(e, n, userMsg_str):
+    userMsg_intArr = [(ord(c)) for c in userMsg_str] #list compression, convert each character to unicode (Decimal)
+    #print(userMsg_intArr)
+    userMsg_hexStr = "0x" + binascii.hexlify(bytearray(userMsg_intArr))
+    #print(userMsg_hexStr)
+    userMsg_bigInt = int(userMsg_hexStr, 16)
+    #print(userMsg_bigInt)
+    return  pow(userMsg_bigInt, e, n)
+
+# Function: Decrypt Message
+    # Input: Private 5 Exponent, Modulus, Message (ciphertext)
+    # Output: Decrypted Message (Plaintext)
+def decrypt(n, d5, ctext):
+    ptext = pow(int(ctext, 10), d5, n)
+    #print(ptext)
+    decode_hexStrRaw = hex(ptext) #string
+    #print(decode_hexStrRaw)
+    decode_hexStr = decode_hexStrRaw[2:-1]
+    #print(decode_hexStr)
+    return bytearray.fromhex(decode_hexStr)
 
 
 
