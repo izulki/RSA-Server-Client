8fb1ca5e1952dc7e23df1c8ca4721370b6a05bcc
Irfan <Irfan@DESKTOP-061P0VF>
2019-03-29 01:57:49 -0300

    Digital signature and verification, communicated with server

--- Client.py
+++ Client.py
@@ -1,19 +1,25 @@
 
-from RSA import generatePrime, generateKey, encrypt, decrypt
+from RSA import generatePrime, generateKey, encrypt, decrypt, sign, verify
 import codecs
 import sys
 import binascii
+import requests
+import time
+import json
 
 print("Welcome to RSA Messaging System!")
 print("****** Client Menu ******")
 print("1. Generate Key Pair")
-print("2. View Key Pair")
-print("3. Encrypt Message")
-print("4. Decrypt Message")
+print("2. Encrypt Message")
+print("3. Decrypt Message")
+print("4. Sign Message")
+print("5. Verify Message")
 
 print("\n****** Network Menu ******")
-print("5. Send Message")
-print("6. Receive Messages")
+print("6. Register onto Network")
+print("7. Send Message")
+print("8. Receive Messages")
+
 
 print("\n****** Debug Menu ******")
 print("100. Generate Prime")
@@ -25,10 +31,10 @@ userOption = input("Your Option: ")
 if (int(userOption) == 1): 
     generateKey(2048)
 
-if (int(userOption) == 3):
+if (int(userOption) == 2):
     print("Encryption for: ") 
-    print("1. Current Client")
-    print("2. Another Client")
+    print("1. This Client")
+    print("2. Another Keypair")
     encryptionOption = input("Your Option: ")
 
     if (encryptionOption == 1):
@@ -45,8 +51,8 @@ if (int(userOption) == 3):
         user_e = input("Enter Public Exponent: ")
         user_n = input("Enter Modulus: ")
 
-if (int(userOption) == 4):
-    print("4 for: ") 
+if (int(userOption) == 3):
+    print("Decrypt for: ") 
     print("1. Current Client")
     print("2. Another Client")
     encryptionOption = input("Your Option: ")
@@ -67,8 +73,98 @@ if (int(userOption) == 4):
         user_e = input("Enter Public Exponent: ")
         user_n = input("Enter Modulus: ")
 
+if (int(userOption) == 4):
+    userMessage = raw_input("Message to sign: ")
+    file = open("n.txt","r")
+    modulus = int(file.read())
+    file.close()
+    file = open("d5.txt","r")
+    d5 = int(file.read())
+    file.close()
+    print(sign(d5, modulus, userMessage))
+
+if (int(userOption) == 5):
+    rawMessage = raw_input("Message to verify: ")
+    signedMessage = raw_input("Signature to verify: ")
+    file = open("n.txt","r")
+    modulus = int(file.read())
+    file.close()
+    print(verify(modulus, 5, signedMessage, rawMessage))
+
     
 
+if (int(userOption) == 6):
+    username = raw_input("Enter Username to Register: ")
+    file = open("username.txt","w") 
+    file.write(username)
+    file.close
+    file = open("n.txt","r")
+    modulus = file.read()
+    file.close()
+    getrequest = requests.get('http://142.93.157.193:3000/register/'+username+'/'+modulus)
 
-   
+
+if (int(userOption) == 7):
+    to = raw_input("Enter Recepient: ")
+    message = raw_input("Enter Message: ")
+    time = time.time()
+    
+    # Get Sender Private Exponent (For Signing)
+    file = open("d5.txt","r")
+    dS = int(file.read())
+    file.close()
+
+    # Get Sender Modulus (For Signing)
+    file = open("n.txt","r")
+    nS = int(file.read())
+    file.close()
+
+    # Get Sender Username
+    file = open("username.txt","r")
+    sender = file.read()
+    file.close()
+
+    #Get Recepient Public Key (Modulus)
+    recpMod = requests.get('http://142.93.157.193:3000/getmod/'+to)
+    nR = recpMod.content #Recepient
+    if (nR == "USER NOT FOUND"):
+        print("USER NOT FOUND")
+    nR = int(nR)
+    eR = 5 #Recepient
+
+    #Encrypt message using recepient public key (eR, nR)
+    cText = encrypt(5, nR, message)
+
+    #Sign Message using sender private key (dS, nS)
+    sText = sign(dS, nS, message)
+
+    msgStruct = {
+    "cText": cText,
+    "sText": sText,
+    "Sender": sender
+    }
+
+    msgJSON = json.dumps(msgStruct)
+
+
+
+    
+
+    print(msgJSON)
+
+
+
+
+    # file = open("username.txt","r")
+    # username = file.read()
+    # file.close()
+
+    # file = open("username.txt","r")
+    # username = file.read()
+    # file.close()
+
+    # file = open("username.txt","r")
+    # username = file.read()
+    # file.close()
+
     
--- RSA.py
+++ RSA.py
@@ -3,6 +3,7 @@ from random import getrandbits, randrange
 from RM import isPrime, rabinMiller
 from RSAMath import egcd, modinv
 import binascii
+import hashlib
 
 #GeneratePrime (RSA) 
 def generatePrime(primeSize): #size of prime in bits 
@@ -96,6 +97,44 @@ def decrypt(n, d5, ctext):
     decode_hexStr = decode_hexStrRaw[2:-1]
     #print(decode_hexStr)
     return bytearray.fromhex(decode_hexStr)
+
+
+# Function: Sign Message
+    # Input: Private Exponent, Modulus, Message (Plaintext)
+    # Output: Encrypted Message (Ciphertext)
+def sign(d, n, userMsg_str):
+    h = hashlib.md5(userMsg_str)
+    #print("Signed Hash: " + h.hexdigest())
+    userMsg_intArr = [(ord(c)) for c in h.hexdigest()] #list compression, convert each character to unicode (Decimal)
+    #print(userMsg_intArr)
+    userMsg_hexStr = "0x" + binascii.hexlify(bytearray(userMsg_intArr))
+    #print(userMsg_hexStr)
+    userMsg_bigInt = int(userMsg_hexStr, 16)
+    #print(userMsg_bigInt)
+    return pow(userMsg_bigInt, d, n)
+
+
+# Function: Decrypt Message
+    # Input: Private 5 Exponent, Modulus, Message (ciphertext)
+    # Output: Decrypted Message (Plaintext)
+def verify(n, e, signedtext, message):
+    ptext = pow(int(signedtext, 10), e, n)
+    #print(ptext)
+    decode_hexStrRaw = hex(ptext) #string
+    #print(decode_hexStrRaw)
+    decode_hexStr = decode_hexStrRaw[2:-1]
+    #print(decode_hexStr)
+    gotHash = bytearray.fromhex(decode_hexStr)
+    print(gotHash)
+    h = hashlib.md5(message)
+    if (gotHash==h.hexdigest()):
+        return True
+    else:
+        return False
+
+  
+
+
 
 
 
